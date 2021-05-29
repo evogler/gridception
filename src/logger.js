8902d60b32826cbdce63789397656fec2d21af43
Eric Vogler <ericvogler@gmail.com>
2021-05-29 08:23:30 -0700

    Create logger

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,64 @@
 class Logger {
   constructor() {
-    this._log = [];
+    this.reset();
+    this._triggers = {
+      // resetTimeCache: 1,
+      // event: 100,
+      // getEventsInWindow: 29,
+    };
   }
 
-  const log(...args) {
-    this._log.push([new Date(), args]);
+  reset() {
+    this._log = [];
+    this._counter = {};
+  }
+
+  log(key, ...val) {
+    this._log.push({ key, val, time: new Date() });
+    this._counter[key] = (this._counter[key] ?? 0) + 1;
+    if (this._triggers[key]) {
+      if (this._counter[key] % this._triggers[key] === 0) {
+        console.log(key, val, this._counter[key]);
+      }
+    }
+  }
+
+  addTrigger(trigger, rate = 1) {
+    this._triggers[trigger] = rate;
+  }
+
+  get(key) {
+    return this._log.filter(x => x.key === key);
+  }
+
+  count(key) {
+    return this._counter[key] ?? 0;
+  }
+
+  report(reset = false) {
+    // const res = {};
+    // for (const { key, val, time } of this._log) {
+    //   res[key] = (res[key] ?? 0) + 1;
+    // }
+    const res = this._counter;
+    if (reset) {
+      this.reset();
+    }
+    return res;
+  }
+
+  printReport(reset = false) {
+    Object.entries(this.report(reset)).forEach(x => console.log(x.join(': ')));
+    console.log(' ');
+  }
+
+  test() {
+    console.log('logger test');
   }
 }
 
 const logger = new Logger();
+logger.log = logger.log.bind(logger);
 
+window.logger = logger;
 module.exports = logger;
